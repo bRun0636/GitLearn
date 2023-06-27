2b33325d83c623a292bf5b53e937d55efa345e33
tenebris <kurk636@gmail.com>
2023-06-27 18:08:40 +0300

    Добавила функцию удаление файла

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-
+def delete_file():
+    file_path = 'file.txt'
+    try:
+        os.remove(file_path)
+        return True
+    except:
+        print("The system cannot find the file specified")
+        return False
