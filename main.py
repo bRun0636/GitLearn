19c17f332b611081120e5de635af5e02f9bd4dc8
bRun0 <mnamikaze660@gmail.com>
2023-06-27 19:25:30 +0300

    Добавил запуск

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
 def write_in_txt_file():
+    a = input("Введите текст:\n")
     with open('file.txt', 'w') as writer:
-        writer.write('I love Git')
+        writer.write(a)
         return True
 
 def delete_file():
@@ -18,3 +20,10 @@ def read_txt_file():
         data = f.read()
         print(data)
         return data
+
+if __name__ == "__main__":
+    if(write_in_txt_file()):
+        print("[ok] i write")
+    print(read_txt_file())
+    delete_file()
+    print("[ok] i delete")
